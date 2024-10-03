de4ee5bb6eaa6442bda876fd614028b352a35b56
Bbypepe <babypepeofficially@gmail.com>
2024-10-03 22:58:48 +0300

    Create auth.js

---
+++ src/auth/auth.js
@@ -0,0 +1,24 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/firebaseConfig";
+
+// Function to create a new user
+export const registerUser = async (email, password) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    console.error("Error registering user: ", error);
+    throw error;
+  }
+};
+
+// Function to sign in a user
+export const loginUser = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    console.error("Error logging in: ", error);
+    throw error;
+  }
+};
